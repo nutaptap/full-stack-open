8dd32ecf6fa50a6db1b3c75aa237dca9b5fda3e7
nutaptap <nutaptap@gmail.com>
2024-05-27 12:08:54 +0200

    Completed exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import Form from "./Form";
 import Numbers from "./Numbers";
+import personsService from "./services/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState(undefined);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    /*     const newPersons = getAll;
+    setPersons(newPersons);
+    console.log(newPersons); */
+    personsService.getAll().then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   function handleName(e) {
     setNewName(e.target.value);
@@ -44,13 +49,19 @@ const App = () => {
   return (
     <main>
       <h1>Phonebook</h1>
-      <Filter handleFilter={handleFilter} />
-      <Form
-        handleName={handleName}
-        handleNumber={handleNumber}
-        handleClick={handleClick}
-      />
-      <Numbers persons={persons} filter={filter} />
+      {persons.length > 0 ? (
+        <div>
+          <Filter handleFilter={handleFilter} />
+          <Form
+            handleName={handleName}
+            handleNumber={handleNumber}
+            handleClick={handleClick}
+          />
+          <Numbers persons={persons} filter={filter} />
+        </div>
+      ) : (
+        <div>loading</div>
+      )}
     </main>
   );
 };
